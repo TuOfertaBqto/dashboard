66d12a25211031a9073a99b8bb2ee918ff360df6
Abraham <1001.28021547.ucla@gmail.com>
2025-07-10 17:12:16 -0400

    add NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ContractsPage from "./pages/ContractsPage";
 import ContractFormPage from "./pages/ContractFormPage";
 import { ProductListPage } from "./pages/ProductListPage";
 import { ProductFormPage } from "./pages/ProductFormPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -36,6 +37,8 @@ function App() {
             <Route path="/products" element={<ProductListPage />} />
             <Route path="/products/new" element={<ProductFormPage />} />
             <Route path="/products/:id/edit" element={<ProductFormPage />} />
+
+            <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
