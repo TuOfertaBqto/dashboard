c63b9d9dbac61b70308d52b88b339dd35fbabec3
Abraham <1001.28021547.ucla@gmail.com>
2025-08-13 12:06:56 -0400

    add btn edit

--- src/components/RequestedCard.tsx
+++ src/components/RequestedCard.tsx
@@ -35,10 +35,7 @@ export function RequestedCard({
   }, 0);
 
   return (
-    <div
-      className="bg-white shadow-md rounded-2xl p-5 transition hover:shadow-lg space-y-4 cursor-pointer h-full flex flex-col"
-      onClick={() => navigate(`/requests/${contract.id}/edit`)}
-    >
+    <div className="bg-white shadow-md rounded-2xl p-5 transition hover:shadow-lg space-y-4 h-full flex flex-col">
       <div>
         <div className="flex items-center justify-between">
           <h2 className="text-lg font-semibold text-gray-800">
@@ -99,82 +96,97 @@ export function RequestedCard({
         </ul>
       </div>
 
-      <div>
-        <p className="text-sm text-gray-400 font-medium">Acuerdo</p>
-        <p className="text-gray-700 font-semibold">
-          {contract.agreement === "weekly" ? "Semanal" : "Quincenal"} - $
-          {amount * (contract.agreement === "weekly" ? 1 : 2)}
-        </p>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-sm text-gray-400 font-medium">Acuerdo</p>
+          <p className="text-gray-700 font-semibold">
+            {contract.agreement === "weekly" ? "Semanal" : "Quincenal"} - $
+            {amount * (contract.agreement === "weekly" ? 1 : 2)}
+          </p>
+        </div>
+
+        <div>
+          <p className="text-sm text-gray-400 font-medium">Total</p>
+          <p className="font-bold text-green-600">${contract.totalPrice}</p>
+        </div>
       </div>
 
-      <div className="pt-2 border-t flex justify-between items-center mt-auto">
-        <span className="text-lg font-bold text-green-600">
-          ${contract.totalPrice}
-        </span>
-        <div className="flex gap-2">
-          {role == "main" && (
-            <>
-              <button
-                className={`px-3 py-1 rounded-md text-white text-sm transition ${
-                  approvingId === contract.id
-                    ? "bg-green-400 cursor-not-allowed"
-                    : "bg-green-600 hover:bg-green-700 cursor-pointer"
-                }`}
-                disabled={approvingId === contract.id}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setApprovingId(contract.id);
-                  onApprove(contract.id).finally(() => {
-                    setApprovingId(null);
-                  });
-                }}
-              >
-                {approvingId === contract.id ? "Aprobando..." : "Aprobar"}
-              </button>
+      {contract.status === "pending" && (
+        <div className="pt-2 border-t flex justify-end items-center mt-auto">
+          <div className="flex gap-2">
+            {(role === "main" || role === "vendor") &&
+              contract.status === "pending" && (
+                <button
+                  className="px-3 py-1 rounded-md text-white text-sm bg-blue-500 cursor-pointer hover:bg-blue-600 transition"
+                  onClick={() => navigate(`/requests/${contract.id}/edit`)}
+                >
+                  Editar
+                </button>
+              )}
+            {role == "main" && (
+              <>
+                <button
+                  className={`px-3 py-1 rounded-md text-white text-sm transition ${
+                    approvingId === contract.id
+                      ? "bg-green-400 cursor-not-allowed"
+                      : "bg-green-600 hover:bg-green-700 cursor-pointer"
+                  }`}
+                  disabled={approvingId === contract.id}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setApprovingId(contract.id);
+                    onApprove(contract.id).finally(() => {
+                      setApprovingId(null);
+                    });
+                  }}
+                >
+                  {approvingId === contract.id ? "Aprobando..." : "Aprobar"}
+                </button>
 
-              <button
-                className={`px-3 py-1 rounded-md text-white text-sm transition 
+                <button
+                  className={`px-3 py-1 rounded-md text-white text-sm transition 
               ${
                 cancellingId === contract.id
                   ? "bg-gray-400 cursor-not-allowed"
                   : "bg-red-500 hover:bg-red-600 cursor-pointer"
               }`}
-                disabled={cancellingId === contract.id}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setCancellingId(contract.id);
-                  onCancel(contract.id).finally(() => {
-                    setCancellingId(null);
-                  });
-                }}
-              >
-                {cancellingId === contract.id ? "Cancelando..." : "Cancelar"}
-              </button>
-            </>
-          )}
+                  disabled={cancellingId === contract.id}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setCancellingId(contract.id);
+                    onCancel(contract.id).finally(() => {
+                      setCancellingId(null);
+                    });
+                  }}
+                >
+                  {cancellingId === contract.id ? "Cancelando..." : "Cancelar"}
+                </button>
+              </>
+            )}
 
-          {role == "vendor" && contract.status === "pending" && (
-            <button
-              className={`px-3 py-1 rounded-md text-white text-sm transition 
+            {role == "vendor" && contract.status === "pending" && (
+              <button
+                className={`px-3 py-1 rounded-md text-white text-sm transition 
             ${
               deletionId === contract.id
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-red-500 hover:bg-red-600 cursor-pointer"
             }`}
-              disabled={deletionId === contract.id}
-              onClick={(e) => {
-                e.stopPropagation();
-                setDeletionId(contract.id);
-                onDelete(contract.id).finally(() => {
-                  setDeletionId(null);
-                });
-              }}
-            >
-              {deletionId === contract.id ? "Eliminando..." : "Eliminar"}
-            </button>
-          )}
+                disabled={deletionId === contract.id}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setDeletionId(contract.id);
+                  onDelete(contract.id).finally(() => {
+                    setDeletionId(null);
+                  });
+                }}
+              >
+                {deletionId === contract.id ? "Eliminando..." : "Eliminar"}
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
