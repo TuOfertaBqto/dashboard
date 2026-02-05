8c71698955de0f19498cc14783987a3bd3839f6a
Abraham <1001.28021547.ucla@gmail.com>
2026-02-05 11:09:27 -0400

    fix name VITE_DOLARVZLA_KEY

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -62,6 +62,8 @@ export const ProductForm = ({ initialData, onSubmit, categories }: Props) => {
   useEffect(() => {
     const fetchPrice = async () => {
       try {
+        console.log(DOLARVZLA_KEY);
+        
         fetch("https://criptoya.com/api/binancep2p/usdt/ves")
           .then((response) => response.json())
           .then((data) => {
